0a02ff264c5ce7a2d26c3fe4f65a406491478451
MichelML <michmoreau.l@gmail.com>
2017-06-01 23:51:01 -0400

    implement full working first version

--- src/jlog/core.clj
+++ src/jlog/core.clj
@@ -1,6 +1,9 @@
 (ns jlog.core
   (:require [clojure.string :as str])
+  (:require [clojure.java.shell :only [sh]])
   (:gen-class))
+
+(use '[clojure.java.shell :only [sh]])
 
 (defn has-length?
   "Checks if a list or string is of length n."
@@ -37,7 +40,23 @@
       (str/replace timelog "h" "h ") 
       timelog)))
 
+(defn jar-path
+  "utility function to get the jar path without the jar file."
+  [& [ns]]
+  (str/replace (-> (or ns (class *ns*))
+      .getProtectionDomain .getCodeSource .getLocation .getPath) #"[^/]+$" ""))
+
+(def jira-issue
+  (future 
+    (let [out (:out (sh "hg" "branch"))]
+      (if-not (str/blank? out) 
+        (re-find #"[a-zA-Z]{3,4}-\d{1,4}" out)
+        (throw (Exception. "Cannot retrieve your current branch. Make sure you are in a repository's."))))))
+
 (defn -main [& args]
   (if (valid-jlog-args-list? (get-two-args args))
-    (spit "jlog.txt" (str (.format (java.text.SimpleDateFormat. "MM/dd/yyyy") (new java.util.Date)) " --- " (space-hrs&mins (second args)) "\n") :append true)
-    (println "Invalid syntax. Run jlog with 'jlog -t 1h'")))
+    (spit (str (jar-path) "jlog.txt") (str @jira-issue " --- " (.format (java.text.SimpleDateFormat. "MM/dd/yyyy") (new java.util.Date)) " --- " (space-hrs&mins (second args)) "\n") :append true)
+    (println "Invalid syntax. Run jlog with 'jlog -t 1h'"))
+  )
+
+
